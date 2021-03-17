43fa093be3496d7420f84afa26711456943a0549
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-03-16 22:20:58 -0300

    criado projeto de lógica, implementado primeiro acesso ao arquivo csv com o type provider

--- RecadastroAMG.Logic/Prototypes/Prototype.fsx
+++ RecadastroAMG.Logic/Prototypes/Prototype.fsx
@@ -1,5 +1,15 @@
-﻿open FSharp.Data
+﻿
+//#r @"C:\Users\JESSE JUNIOR\.nuget\packages\fsharp.data\4.0.1\lib\netstandard2.0\FSharp.Data.dll"
+//#r @"C:\Users\JESSE JUNIOR\.nuget\packages\fsharp.data\4.0.1\typeproviders\fsharp41\netstandard2.0\FSharp.Data.DesignTime.dll"
+#r "nuget:FSharp.Data"
+
+open FSharp.Data
+
+type DadosAtuais = FSharp.Data.CsvProvider<"D:/repos/amg/associados.csv",";">
 
 
+let dados = DadosAtuais.GetSample()
 
-let dadoAtual = CsvProvider
+dados.Skip(14).Take(2)
+
+
