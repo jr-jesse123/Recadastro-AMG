e3c6c8ef28f8c8959218d4d0af2978d8b34bef2f
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-03-18 18:03:04 -0300

    arquivos estruturados para fs

--- RecadastroAMG.Logic/Configuration.fs
+++ RecadastroAMG.Logic/Configuration.fs
@@ -1,20 +1,20 @@
 ﻿namespace Config
 open FSharp.Data.JsonProvider
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Configuration
+open System
+open CEPAberto
+
 module Configuration =
     type Configurations = JsonProvider<"./appsettings.json","Config">
     let DefaultConfiguration =  Configurations.GetSample()
-    
+ 
 
-
     
+    //let host = 
+    //    let Host = HostBuilder().ConfigureAppConfiguration 
+    //                (fun x y -> ignore <| y.AddUserSecrets<teste>()  )
         
-
-
-
-
-
-
-
-
-
-
+    //    Host.Build().Run()
+    //    Host
+        
