cfda5696c87184c480930f6d457147d28713bcf0
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-03-23 20:27:38 -0300

    adicionado projeto de testes

---
+++ FormTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using RecadastroAMG.Web.Pages;
+using System;
+using Xunit;
+using Bunit;
+using Index = RecadastroAMG.Web.Pages.Index;
+
+namespace FormTests
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
